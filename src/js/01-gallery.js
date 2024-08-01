f1a98bd789ea6ab7bf905f707b831adefeb518d1
Adelina515 <adelgodlevska@gmail.com>
2024-08-01 17:30:42 +0300

    add task 2

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,4 +1,55 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const galleryUl = document.querySelector(".gallery");
 
+const markup = galleryItems
+  .map(
+    ({ preview, original, description }) =>
+      `<li class="gallery__item">
+  <a class="gallery__link" href=${original}>
+    <img
+      class="gallery__image"
+      src=${preview}
+      data-source=${original}
+      alt=${description}
+    />
+  </a>
+</li>`
+  )
+  .join("");
 console.log(galleryItems);
+
+galleryUl.insertAdjacentHTML("beforeend", markup);
+let instance = null;
+
+const handleModal = (ev) => {
+  ev.preventDefault();
+  if (ev.target.nodeName !== "IMG") return;
+  const imgModal = ev.target.dataset.source;
+  console.log("target", imgModal);
+  instance = basicLightbox.create(
+    `
+      <div class="modal">
+      <img
+      class="gallery__imageL"
+      src=${imgModal}
+      width="800" height="600"
+    />
+      </div>
+  `,
+    {
+      onClose: () => {
+        document.removeEventListener("keydown", handleKeydown);
+      },
+    }
+  );
+
+  instance.show();
+  document.addEventListener("keydown", handleKeydown);
+};
+const handleKeydown = (ev) => {
+  if (ev.key === "Escape" && instance) {
+    instance.close();
+  }
+};
+galleryUl.addEventListener("click", handleModal);
--- src/js/02-lightbox.js
+++ src/js/02-lightbox.js
@@ -1,4 +1,33 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryUl = document.querySelector(".gallery");
+
+const markup = galleryItems
+  .map(
+    ({ preview, original, description }) =>
+      `<li class="gallery__item">
+   <a class="gallery__link" href=${original}>
+      <img class="gallery__image" src=${preview} alt=${description} />
+   </a>
+</li>`
+  )
+  .join("");
+
+galleryUl.insertAdjacentHTML("beforeend", markup);
+
+let lightbox = new SimpleLightbox(".gallery a", {
+  captions: true,
+  captionsData: "alt",
+  captionPosition: "bottom",
+  captionDelay: 250,
+});
+
+// Ініціалізація SimpleLightbox: Після того, як розмітка додана до DOM,
+//  створюється новий екземпляр SimpleLightbox(let lightbox).Він приймає
+// селектор, який вказує на всі посилання у галереї('.gallery a'), та об'єкт
+// налаштувань.Налаштування включають: відображення підписів, джерело підпису з
+// атрибута alt, розташування підпису знизу, затримка перед відображенням
+// підпису — 250 мс.
